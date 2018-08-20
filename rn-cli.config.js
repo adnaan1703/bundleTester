3c8c4fd01cd208c7f1a36f0a14b43a61afdeb5d8
adnaan.ahmed <adnaan.ahmed@flipkart.com>
2018-08-20 18:02:10 +0530

    init

---
+++ rn-cli.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    resolver: {
+        platforms: ['custom']
+    },
+    watch: false,
+    resetCache: true,
+    createModuleIdFactory: () => (path) => {
+        let hash = 0, i, chr, len;
+        if (this.length === 0) return hash;
+        for (i = 0, len = path.length; i < len; i++) {
+            chr = path.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0;
+        }
+        console.log(hash);
+        return hash;
+    },
+    reporter: {
+        update: () => {
+            console.log('please log this, please!')
+        }
+    }
+};

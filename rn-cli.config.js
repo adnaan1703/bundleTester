9c049dd2a69be77c431a49499bb11ec67ea92cc3
adnaan.ahmed <adnaan.ahmed@flipkart.com>
2018-08-21 16:06:38 +0530

    added react-native-cli and metro packages.

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,20 +1,12 @@
 module.exports = {
-    watch: false,
-    resetCache: true,
-    createModuleIdFactory: () => (path) => {
-        let hash = 0, i, chr, len;
-        if (this.length === 0) return hash;
-        for (i = 0, len = path.length; i < len; i++) {
-            chr = path.charCodeAt(i);
-            hash = ((hash << 5) - hash) + chr;
-            hash |= 0;
-        }
-        console.log(hash);
-        return hash;
+    getPlatforms() {
+        return ['custom'];
     },
-    reporter: {
-        update: () => {
-            console.log('please log this, please!')
+    getReporter() {
+        return {
+            update: function () {
+                console.log('please console this');
+            }
         }
     }
 };
